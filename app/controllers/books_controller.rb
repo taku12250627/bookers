d2631cb190671db8881d7cf6797ca7a44a7ed68c
EC2 Default User <ec2-user@ip-172-31-12-41.ec2.internal>
2022-02-09 09:14:00 +0000

    エラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id) , notice: 'Book was successfully updated.'
     else
       render :edit
     end
